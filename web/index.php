9bcf4d403a57f13ff1fba74a781b50accb624347
uucyan <uuunouucyan@gmail.com>
2016-06-15 23:34:05 +0900

    create master

---
+++ web/index.php
@@ -0,0 +1,54 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+$app->get('/', function() {
+    return "Hello World!";
+});
+
+$app->get('/test/{test}', function($test) use($app) {
+
+    $test1 = 300;
+    $test1 = $test1 + 1000;
+    $test2 =& $test1;
+    define("TEST3", 12345);
+
+    $a = "値１";
+    $b = "値２";
+    $c = "値３";
+    $index = 0;
+    $array = array('','','');
+
+
+    foreach ($array as &$arr) {
+      switch ($index) {
+        case 0:
+          $arr = $a;
+          break;
+        case 1:
+          $arr = $b;
+          break;
+        case 2:
+          $arr = $c;
+          break;
+      }
+      $index ++;
+    }
+
+    return $app['twig']->render('test.twig', array(
+        'test' => $test,
+        'test1' => "$test1",
+        'test2' => "$test2",
+        'array' => "$array[2]",
+    ));
+});
+
+$app->get('/hello/{name}', function($name) use($app) {
+    return $app['twig']->render('hello.twig', array(
+        'name' => $name,
+    ));
+});
+$app->run();
