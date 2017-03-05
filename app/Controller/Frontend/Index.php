afd9622ed483b6b1ca82ac24526063ce21943c55
uucyan <uuunouucyan@gmail.com>
2017-03-06 03:04:21 +0900

    [frontend]Topページにキューブのやつ追加

--- app/Controller/Frontend/Index.php
+++ app/Controller/Frontend/Index.php
@@ -2,10 +2,16 @@
 namespace app\Controller\Frontend;
 
 use Silex\Application;
+use App\Model\Member;
 
 class Index
 {
     public function indexAction(Application $app){
-        return $app['twig']->render('frontend\index.twig', array());
+        $member = new Member($app);
+        $memberDatas = $member->getMembers();
+
+        return $app['twig']->render('frontend\index.twig', array(
+            'members' => $memberDatas,
+        ));
     }
 }
